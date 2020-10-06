542878c4cb1b52aabcf13467ba9479ece37b5a68
felixyunianto <felixyunianto@gmail.com>
2020-10-06 18:51:54 +0700

    update image

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ function Spinner() {
     var deg = Math.floor(Math.random() * (x - y) + y);
     document.querySelector('.box').style.transform = "rotate(" + deg + "deg)"
     document.querySelector('.image-results').src = './img/tanda_tanya.png'
+    document.querySelector('#design').innerHTML = ''
 }
 document.querySelector('#image1').addEventListener('click', function () {
+    document.querySelector('#design').innerHTML = '1. Design Satu'
     document.querySelector('.image-results').src = './img/MacBook - 1.png'
 })
 
 document.querySelector('#image2').addEventListener('click', function () {
-    document.querySelector('.image-results').src = './img/MacBook - 2.png'
+    document.querySelector('#design').innerHTML = '2. Design Dua'
+    document.querySelector('.image-results').src = './img/MacBook - 3.png'
 })
 
 document.querySelector('#image3').addEventListener('click', function () {
@@ -19,15 +22,18 @@ document.querySelector('#image3').addEventListener('click', function () {
 })
 
 document.querySelector('#image4').addEventListener('click', function () {
-    document.querySelector('.image-results').src = './img/MacBook - 3.png'
+    document.querySelector('#design').innerHTML = '3. Design Tiga'
+    document.querySelector('.image-results').src = './img/MacBook - 5.png'
 })
 
 document.querySelector('#image5').addEventListener('click', function () {
-    document.querySelector('.image-results').src = './img/MacBook - 4.png'
+    document.querySelector('#design').innerHTML = '4. Design Empat'
+    document.querySelector('.image-results').src = './img/MacBook - 2.png'
 })
 
 document.querySelector('#image6').addEventListener('click', function () {
-    document.querySelector('.image-results').src = './img/MacBook - 5.png'
+    document.querySelector('#design').innerHTML = '5. Design Lima'
+    document.querySelector('.image-results').src = './img/MacBook - 4.png'
 })
 
 document.querySelector('#image7').addEventListener('click', function () {
@@ -35,5 +41,6 @@ document.querySelector('#image7').addEventListener('click', function () {
 })
 
 document.querySelector('#image8').addEventListener('click', function () {
+    document.querySelector('#design').innerHTML = '6. Design Enam'
     document.querySelector('.image-results').src = './img/MacBook - 6.png'
 })
